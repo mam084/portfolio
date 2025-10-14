e79963fc14a49792dff68557a13ea9cf8ae172d7
mam084 <mam084@ucsd.edu>
2025-10-14 16:32:04 -0700

    JS Navigation

--- global.js
+++ global.js
@@ -1,15 +1,37 @@
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+    return Array.from(context.querySelectorAll(selector));
 }
 
 navLinks = SS("nav a")
 
 let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
+    (a) => a.host === location.host && a.pathname === location.pathname,
 );
 
 if (currentLink) {
-  currentLink.classList.add('current');
+    currentLink.classList.add('current');
 }
+
+let pages = [
+    { url: 'https://mam084.github.io/portfolio/', title: 'Home' },
+    { url: 'https://mam084.github.io/portfolio/projects/', title: 'Projects' },
+    { url: 'https://mam084.github.io/portfolio/CV', title: 'Resume' },
+    { url: 'https://mam084.github.io/portfolio/projects/contact', title: 'Contact' },
+    { url: 'https://github.com/mam084', title: 'GitHub Profile' },
+];
+
+const BASE_PATH = location.hostname === 'localhost' || location.hostname === '127.0.0.1'? '/' : '/website/'; // GitHub Pages repo name
+
+url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
