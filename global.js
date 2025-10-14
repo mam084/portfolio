7fac2360604720748e2d052570717f4c8d86fd22
mam084 <mam084@ucsd.edu>
2025-10-14 16:42:29 -0700

    Some JS reformatting

--- global.js
+++ global.js
@@ -4,15 +4,6 @@ function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
 
-navLinks = $$("nav a")
-
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-);
-
-if (currentLink) {
-    currentLink.classList.add('current');
-}
 
 let pages = [
     { url: 'https://mam084.github.io/portfolio/', title: 'Home' },
@@ -24,14 +15,26 @@ let pages = [
 
 const BASE_PATH = location.hostname === 'localhost' || location.hostname === '127.0.0.1'? '/' : '/website/'; // GitHub Pages repo name
 
-url = !url.startsWith('http') ? BASE_PATH + url : url;
-
 let nav = document.createElement('nav');
 document.body.prepend(nav);
+
 
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
+
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
 
+
+navLinks = $$("nav a")
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+if (currentLink) {
+    currentLink.classList.add('current');
+}
