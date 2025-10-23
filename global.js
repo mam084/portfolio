6fe707b7ff43dcc9bbf168c916f8a465d7690302
mam084 <mam084@ucsd.edu>
2025-10-23 12:14:51 -0700

    Adding json for projects

--- global.js
+++ global.js
@@ -76,3 +76,30 @@ mql.addEventListener?.("change", () => {
   }
 });
 
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+    
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  // write javascript that will allow dynamic heading levels based on previous function
+  containerElement.innerHTML = '';
+  const article = document.createElement('article');
+  article.innerHTML = `
+    <headingLevel>${project.title}</headingLevel>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+  `;
+  containerElement.appendChild(article);
+
+}
